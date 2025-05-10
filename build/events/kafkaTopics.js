21a0acebef9a048a7d1a07dbc80da94f120c0c6d
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-10 11:10:44 +0530

    chore: build

---
+++ build/events/kafkaTopics.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.KafkaTopics = void 0;
+var KafkaTopics;
+(function (KafkaTopics) {
+    KafkaTopics["USER_PASSWORD_RECOVERY_EVENTS_TOPIC"] = "user.password-recovery-request.events";
+    KafkaTopics["USER_SINGUP_EVENTS_TOPIC"] = " user.signup.events";
+})(KafkaTopics || (exports.KafkaTopics = KafkaTopics = {}));
