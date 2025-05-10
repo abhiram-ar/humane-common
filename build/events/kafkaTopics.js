8bc8ebc260af1adde8f39beb71dd5d579222ed42
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-10 12:17:46 +0530

    chore: build

--- build/events/kafkaTopics.js
+++ build/events/kafkaTopics.js
@@ -4,5 +4,5 @@ exports.KafkaTopics = void 0;
 var KafkaTopics;
 (function (KafkaTopics) {
     KafkaTopics["USER_PASSWORD_RECOVERY_EVENTS_TOPIC"] = "user.password-recovery-request.events";
-    KafkaTopics["USER_SINGUP_EVENTS_TOPIC"] = " user.signup.events";
+    KafkaTopics["USER_SINGUP_EVENTS_TOPIC"] = "user.signup.events";
 })(KafkaTopics || (exports.KafkaTopics = KafkaTopics = {}));
--- src/events/kafkaTopics.ts
+++ src/events/kafkaTopics.ts
@@ -1,4 +1,4 @@
 export enum KafkaTopics {
    USER_PASSWORD_RECOVERY_EVENTS_TOPIC = 'user.password-recovery-request.events',
-   USER_SINGUP_EVENTS_TOPIC = ' user.signup.events',
+   USER_SINGUP_EVENTS_TOPIC = 'user.signup.events',
 }
