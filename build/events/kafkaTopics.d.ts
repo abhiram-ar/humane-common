536549bfe3ffc5108da41c941cfbeab8edb32831
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-20 13:58:26 +0530

    chore: build

--- build/events/kafkaTopics.d.ts
+++ build/events/kafkaTopics.d.ts
@@ -1,5 +1,5 @@
-export declare enum KafkaTopics {
-    USER_PASSWORD_RECOVERY_EVENTS_TOPIC = "user.password-recovery-request.events",
-    USER_SINGUP_EVENTS_TOPIC = "user.signup.events",
-    USER_PROFILE_EVENTS_TOPIC = "user.profile.events"
-}
+export declare const KafkaTopics: {
+    USER_PASSWORD_RECOVERY_EVENTS_TOPIC: string;
+    USER_SINGUP_EVENTS_TOPIC: string;
+    USER_PROFILE_EVENTS_TOPIC: string;
+};
--- build/events/kafkaTopics.js
+++ build/events/kafkaTopics.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.KafkaTopics = void 0;
-var KafkaTopics;
-(function (KafkaTopics) {
-    KafkaTopics["USER_PASSWORD_RECOVERY_EVENTS_TOPIC"] = "user.password-recovery-request.events";
-    KafkaTopics["USER_SINGUP_EVENTS_TOPIC"] = "user.signup.events";
-    KafkaTopics["USER_PROFILE_EVENTS_TOPIC"] = "user.profile.events";
-})(KafkaTopics || (exports.KafkaTopics = KafkaTopics = {}));
+exports.KafkaTopics = {
+    USER_PASSWORD_RECOVERY_EVENTS_TOPIC: 'user.password-recovery-request.events',
+    USER_SINGUP_EVENTS_TOPIC: 'user.signup.events',
+    USER_PROFILE_EVENTS_TOPIC: 'user.profile.events',
+};
