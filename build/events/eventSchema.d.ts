16d2b4cc51650ef25a2150e7dcabfc1efcb3d5af
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-21 17:22:17 +0530

    clean

--- build/events/eventSchema.d.ts
+++
@@ -1,9 +0,0 @@
-export type RootAppEvent<eventType, payloadType> = {
-    eventType: eventType;
-    version: number;
-    eventId: string;
-    source: string;
-    timestamp: string;
-    traceId?: string;
-    payload: payloadType;
-};
