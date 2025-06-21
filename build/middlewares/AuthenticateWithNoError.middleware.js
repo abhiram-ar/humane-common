3f56cd866177ea3b8e35644fe093e0f1e84a7557
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-21 19:44:20 +0530

    chore: build

---
+++ build/middlewares/AuthenticateWithNoError.middleware.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthenticateWithNoError = void 0;
+const config_1 = require("../utils/jwt/config");
+const AuthenticateWithNoError = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return next();
+    }
+    const token = authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(' ')[1];
+    if (!token) {
+        return next();
+    }
+    const payload = config_1.verifyAccessToken.execute(token);
+    if (payload.type === 'user' || payload.type === 'admin') {
+        req.user = payload;
+    }
+    return next();
+};
+exports.AuthenticateWithNoError = AuthenticateWithNoError;
---
+++ src/middlewares/AuthenticateWithNoError.middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { JWTTokenPaylod } from '../Types/JWTTokenPayload.type';
+import { verifyAccessToken } from '../utils/jwt/config';
+
+declare global {
+   namespace Express {
+      interface Request {
+         user?: JWTTokenPaylod;
+      }
+   }
+}
+
+export const AuthenticateWithNoError = (req: Request, res: Response, next: NextFunction) => {
+   const authHeader = req.headers.authorization;
+   if (!authHeader) {
+      return next();
+   }
+
+   const token = authHeader?.split(' ')[1];
+   if (!token) {
+      return next();
+   }
+
+   const payload = verifyAccessToken.execute(token);
+
+   if (payload.type === 'user' || payload.type === 'admin') {
+      req.user = payload;
+   }
+   return next();
+};
