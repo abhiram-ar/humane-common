6e57c526dae8544dabec3249dffa9adb89b4a9a8
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-18 19:47:46 +0530

    npm run release

--- src/Types/JWTTokenPayload.type.ts
+++ src/Types/JWTTokenPayload.type.ts
@@ -1,11 +1,13 @@
 export type UserJWTTokenPayload = {
    userId: string;
    type: 'user';
+   iss: 'humane';
 };
 
 export type AdminJWTTokenPaylod = {
    adminId: string;
    type: 'admin';
+   iss: 'humane';
 };
 
 export type JWTTokenPaylod = AdminJWTTokenPaylod | UserJWTTokenPayload;
